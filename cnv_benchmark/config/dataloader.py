eaae2c7a06ad07ea3a93129dcea2d3cb9bb4f178
feiler98 <werner.feiler@ukmuenster.de>
2025-08-15 10:40:51 +0200

    first commit

---
+++ cnv_benchmark/config/dataloader.py
@@ -0,0 +1,33 @@
+"""
+------------------------------------------------------------------------------------------------------------------------
+
+DATALOADER
+----------
+
+Handling of the Benchmarking Data for RNA InferCNV methods.
+Configuration file [fetch_data.ini] handles the location of the benchmarking data.
+------------------------------------------------------------------------------------------------------------------------
+
+"""
+
+# imports
+# ______________________________________________________________________________________________________________________
+import pandas as pd
+from pathlib import Path
+# ______________________________________________________________________________________________________________________
+
+class DataLoader:
+
+    def __init__(self):
+        pass
+
+    # getting the necessary config information
+
+
+    @classmethod
+    def fetch_data(cls):
+        pass
+
+    @staticmethod
+    def available_datasets():
+        pass
